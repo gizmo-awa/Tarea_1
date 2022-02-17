41e6e97b6e5c555b9e973d57b21443be3212c039
Alexia Gizmo <alexiagizmo@MacBook-Pro-de-Alexia-2.local>
2022-02-17 10:20:37 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/com/example/tarea_1/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.tarea_1
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
+        textView = findViewById<EditText>(R.id.textView)
+
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onCLick")
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                //userInput.setText("")
+                userInput.text.clear()
+            }
+        })
+    }
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        Log.d(TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+}
